2be587ffc92169e9060743a9c1de63810d56c15c
Desire Kouadio <desirek43@gmail.com>
2023-01-22 00:09:28 +0100

    workflows

--- file_test.py
+++ file_test.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 def test_calc_addition():
     # Fonction test du résultat de 2+4
     output = 2 + 4
@@ -18,7 +15,7 @@ def test_calc_multiply():
     output = 2 * 4
     assert output == 8
 
-    
+
 def test_coucou():
     # Fonction test si la résultat renvoie 'hello'
     output='hello'
